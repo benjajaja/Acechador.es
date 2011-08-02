6aa5cd1d1ea4165995e280e041cc894cc7bbb2d3
Benjamin Grosse <ste3ls@gmail.com>
2011-08-03 01:56:29 +0200

    fix rss, cachetag

--- rss.js
+++ rss.js
@@ -13,7 +13,7 @@ module.exports = function(app, db) {
 	app.all('/rss', handle);
 	
 	var getRssXml = function(rows) {
-		var formatter = require('./datehelper');
+		var formatter = require('./modules/datehelper');
 		var string = '<?xml version="1.0" encoding="utf-8"?>';
 		string += "<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\">\
   <channel>\
