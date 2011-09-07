c2f531318e7b921c3e9b7ffae19c432900473f6c
Benjamin Grosse <ste3ls@gmail.com>
2011-09-08 00:27:47 +0200

    do not publish links with empty href to facebook

--- modules/cronjobs/facebook.js
+++ modules/cronjobs/facebook.js
@@ -1,10 +1,32 @@
+function makeFbPost(baseUrl, imageUrl, link) {
+	if (link.href) {
+		throw new Error('Cannot create facebook post without link.href');
+	}
+	var post = {
+		name: link.name,
+		link: baseUrl+link.href,
+		caption: 'enviado por ',
+		message: ''
+	};
+	
+	if (link.submitter_type == 2) {
+		post.caption += link.username;
+	} else {
+		post.caption += link.submitter;
+	}
+	//post.message += ' en '
+
+	if (link.video_site == 2) {
+		post.picture = imageUrl+'/img/videos/thumb_'+link.id+'.jpg';
+	}
+	return post;
+}
 
 module.exports = function(db, layout, options) {
 	var fb = require('../facebook');
 	var provider = require('../linklistprovider')(db);
 	return {
 		run: function() {
-			console.log('chicking if there\'s something to publish to facebook...');
 			provider.getLinks({
 						limit: 10
 					}, function(links) {
@@ -15,41 +37,30 @@ module.exports = function(db, layout, options) {
 					fb.accessToken(options.appid, options.appsecret,
 							function(err, accessToken) {
 						if (err) {
-							res.send(err);
+							console.error(err);
 						} else {
 							
-							var post = {
-								name: link.name,
-								link: layout.urls.base+link.href,
-								caption: 'enviado por ',
-								message: ''
-							};
-							
-							if (link.submitter_type == 2) {
-								post.caption += link.username;
-							} else {
-								post.caption += link.submitter;
+							var post;
+							try {
+								post = makeFbPost(layout.urls.base, layout.urls.static, link);
+								fb.publish(accessToken, options.pageid, post, function(err, id) {
+									if (err) {
+										console.log(err);
+									} else {
+										db.updateLink(link.id, {'fbpost': id}, function(err) {
+											if (err) {
+												console.error(err);
+											} else {
+												console.log('published: '+id);
+											}
+										});
+										
+									}
+								});
+							} catch(e) {
+								console.error(e);
+								
 							}
-							//post.message += ' en '
-
-							if (link.video_site == 2) {
-								post.picture = layout.urls.static+'/img/videos/thumb_'+link.id+'.jpg';
-							}
-
-							fb.publish(accessToken, options.pageid, post, function(err, id) {
-								if (err) {
-									console.log(err);
-								} else {
-									db.updateLink(link.id, {'fbpost': id}, function(err) {
-										if (err) {
-											console.log(err);
-										} else {
-											console.log('published: '+id);
-										}
-									});
-									
-								}
-							});
 						}
 					});
 				} else {
